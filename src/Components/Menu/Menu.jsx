b73369ea6c94537f8d214edfb15b05d730fff64b
rs_rahaman <jesminakter.rahaman@gmail.comgit config --global user.email jesminakter.rahaman@gmail.com>
2023-04-12 19:49:23 +0600

    some edit and fynaly complited the work

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -49,28 +49,8 @@ const Menu = () => {
           <h2>Dream Jobs</h2>
         </Link>
       </div>
-      {/* <button className="menu__toggle" onClick={toggleMenu}>
-        {isOpen ? "X" : "menu"}
-      </button> */}
       <button className="menu__toggle" onClick={toggleMenu}>
-        {isOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        ) : (
-          "menu"
-        )}
+        {isOpen ? "X " : "menu"}
       </button>
 
       <div className="">
@@ -91,7 +71,7 @@ const Menu = () => {
           </li>
         </ul>
       </div>
-      <div className="btn">
+      <div className="btnMenu">
         <button>Star Applying</button>
       </div>
     </nav>
